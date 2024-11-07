ad2c6258bfcc2af2b21e862b76278dd9cdaa2f39
Vinay KS <vinayks8055@gmail.com>
2024-11-08 00:13:50 +0530

    organising files

---
+++ template_v1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define VI                 vector<int>
+#define VVI                vector<vector<int>>
+#define FOR(I,start, end)  for(int I=start; I<end; I++)
+
+#define XX     int aaa = 2;
+
+#define NL printf("\n")
+
+void printVector(vector<int> arr){
+    for(auto ele : arr){
+        cout<< ele << " ";
+    }
+}
+
+void swapVar(int* a, int* b){
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+//================================================================================
+
+void hello3(){
+
+    printf("\n============START==============\n");
+
+    string ans = "asdfgh";
+    string temp = "";
+
+    ans.pop_back();
+
+    char ch = 'w';
+    cout<<"xx "<<ans +temp<<" xx";
+
+
+    printf("\n++====+++=== DONE ===+++====++\n");
+}
+
+int main(){
+    hello3();
+
+    return 0;
+}
