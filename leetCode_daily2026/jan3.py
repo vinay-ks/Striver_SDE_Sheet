914578e1e1afce617dd3a085fb4ef16fa13ccd88
Vinay KS <vinayks8055@gmail.com>
2026-01-03 20:29:31 +0530

    jan3

---
+++ leetCode_daily2026/jan3.py
@@ -0,0 +1,13 @@
+def numOfWays(self, n: int) -> int:
+    MOD = 10**9 + 7
+
+    # Base case: n = 1
+    two = 6      # ABA patterns
+    three = 6    # ABC patterns
+
+    for _ in range(2, n + 1):
+        new_two = (two * 3 + three * 2) % MOD
+        new_three = (two * 2 + three * 2) % MOD
+        two, three = new_two, new_three
+
+    return (two + three) % MOD
